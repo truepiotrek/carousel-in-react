5418fbc310fc6c138108e181efcac5a4fbf20e92
Piotr Slezak <piotrek.slezak@gmail.com>
2021-03-10 16:34:54 +0100

    set initial classes for pictures and event listeners

--- scripts.js
+++ scripts.js
@@ -1,5 +1,24 @@
 !(function(d) {
 
-    
+    const itemClassName = "carousel-picture";
+    let items = d.getElementsByClassName(itemClassName);
+    let totalItems = items.length;
+    let slide = 0;
+    let moving = true;
+
+    function setInitialClasses() {
+        items[totalItems - 1].classList.add("prev");
+        items[0].classList.add("active");
+        items[1].classList.add("next");
+    }
+
+    function setEventListeners() {
+        let next = d.getElementsByClassName("carousel-button-next");
+        let prev = d.getElementsByClassName("carousel-button-prev");
+
+        next.addEventListener('click', moveNext);
+        prev.addEventListener('click', movePrev);
+    }
+
 
 }(document));
